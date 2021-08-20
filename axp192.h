6b38ef0a4b6f3bc99b59ebd5971a5df6f15acde6
Mika Tuupola <tuupola@appelsiini.net>
2021-08-20 16:12:06 +0300

    Add ioctl support for setting GPIO2 level
    For M5Stack Core2 this enables and disables the speaker amplifier.
    Not properly tested at the moment.
    
    axp192_ioctl(&axp, AXP192_GPIO2_SET_LEVEL, AXP192_HIGH);
    axp192_ioctl(&axp, AXP192_GPIO2_SET_LEVEL, AXP192_LOW);

--- axp192.h
+++ axp192.h
@@ -159,6 +159,7 @@ extern "C" {
 #define AXP192_HIGH                     (&(uint8_t){1})
 
 #define AXP192_GPIO1_SET_LEVEL          (0x9401)
+#define AXP192_GPIO2_SET_LEVEL          (0x9402)
 #define AXP192_GPIO4_SET_LEVEL          (0x9601)
 
 /* Error codes */
