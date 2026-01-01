cba905e17828b42d69bbd9427fcba413a3b3eac7
Mika Tuupola <tuupola@appelsiini.net>
2026-01-01 17:38:48 +0200

    Add test for invalid DCDC2 voltage

--- tests/unit.c
+++ tests/unit.c
@@ -134,6 +134,22 @@ should_fail_invalid_dcdc1_voltage(void)
 }
 
 TEST
+should_fail_invalid_dcdc2_voltage(void)
+{
+    axp192_t axp;
+
+    axp.read = &mock_i2c_read;
+    axp.write = &mock_i2c_write;
+
+    /* Below minimum */
+    ASSERT(AXP192_ERROR_EINVAL == axp192_ioctl(&axp, AXP192_DCDC2_SET_VOLTAGE, 699));
+    /* Above maximum */
+    ASSERT(AXP192_ERROR_EINVAL == axp192_ioctl(&axp, AXP192_DCDC2_SET_VOLTAGE, 2276));
+
+    PASS();
+}
+
+TEST
 should_fail_invalid_ioctl_command(void)
 {
     axp192_t axp;
@@ -145,7 +161,6 @@ should_fail_invalid_ioctl_command(void)
 
     PASS();
 }
-
 
 TEST
 should_enable_dcdc1(void)
@@ -196,6 +211,7 @@ main(int argc, char **argv)
     RUN_TEST(should_set_dcdc1_voltage);
     RUN_TEST(should_fail_invalid_dcdc1_voltage);
     RUN_TEST(should_fail_invalid_ioctl_command);
+    RUN_TEST(should_fail_invalid_dcdc2_voltage);
     RUN_TEST(should_enable_dcdc1);
     RUN_TEST(should_disable_dcdc1);
 
